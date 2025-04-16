798c42d3924007585d4e84f3dc691c520a779c6b
eckofox1981 <franklineck@pm.me>
2025-04-16 15:14:04 +0200

    added files for coment package

---
+++ EckoOfThFox-Backend/src/main/java/efgroup/EckoOfTheFox_backend/comment/CommentController.java
@@ -0,0 +1,18 @@
+package efgroup.EckoOfTheFox_backend.comment;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    //get
+
+    //post
+
+    //update
+
+    //delete
+}
---
+++ EckoOfThFox-Backend/src/main/java/efgroup/EckoOfTheFox_backend/comment/CommentService.java
@@ -0,0 +1,10 @@
+package efgroup.EckoOfTheFox_backend.comment;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+    private final CommentRepository commentRepository;
+}
